623c72e30efff5428fd5a58d58453680f84db943
GADO89 <gadomahmoud377@gmail.com>
2024-01-29 16:47:45 +0200

    RepositoryRestResource@

--- src/main/java/com/spring/playerapi/dao/PlayerDAO.java
+++ src/main/java/com/spring/playerapi/dao/PlayerDAO.java
@@ -2,8 +2,10 @@ package com.spring.playerapi.dao;
 
 import com.spring.playerapi.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+@RepositoryRestResource(path = "persons")
 public interface PlayerDAO extends JpaRepository<Player,Integer> {
 
 
