9d67b5e60db430d4c9f5fce633cf3fd05d2a6afc
GADO89 <gadomahmoud377@gmail.com>
2024-01-29 17:37:57 +0200

    Declaring Interfaces

--- src/main/java/com/spring/playerapi/dao/PlayerDAO.java
+++ src/main/java/com/spring/playerapi/dao/PlayerDAO.java
@@ -3,11 +3,17 @@ package com.spring.playerapi.dao;
 import com.spring.playerapi.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-@RepositoryRestResource(path = "persons")
+//@RepositoryRestResource(path = "persons")
 public interface PlayerDAO extends JpaRepository<Player,Integer> {
 
+    Player findByName(@RequestParam String name);
+    List<Player> findByNameContaining(@RequestParam String name);
+
+
+    // http://localhost:8080/api/players/search/findByNameContaining?name=Ahmed
 
     //public List<Player> getPlayers();
 
